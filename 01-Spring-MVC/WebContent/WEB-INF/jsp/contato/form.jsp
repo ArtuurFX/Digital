0f24a5e3526a22b424dff7434560809e1c602d6f
Logon Aluno <logonrmlocal@fiap.com.br>
2018-08-02 20:48:31 -0300

    aula 1 spring

---
+++ 01-Spring-MVC/WebContent/WEB-INF/jsp/contato/form.jsp
@@ -0,0 +1,29 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<title>Formulario de Contato</title>
+</head>
+<body>
+
+	<h1>Contato</h1>
+	<form action="enviar" method="post">
+		<div>
+			<label for="id-nome">Nome</label>
+			<input type="text" name="nome" id="id-nome">
+		</div>
+		<div>
+			<label for="id-email">E-Mail</label>
+			<input type="text" name="email" id="id-email">
+		</div>
+		<div>
+			<label for="id-tel">Telefone</label>
+			<input type="text" name="telefone" id="id=tel">
+		</div>
+		<input type="submit" value="Enviar">
+	</form>
+	
+</body>
+</html>
---
+++ 01-Spring-MVC/src/br/com/fiap/spring/controller/ContatoController.java
@@ -0,0 +1,36 @@
+package br.com.fiap.spring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class ContatoController {
+
+	//Metodo que abre a pagina cadastrar
+	@RequestMapping("enviar")
+	public String abrirForm() {
+		return "contato/form";
+	}
+	
+	//Metodo que processa as informações
+	@RequestMapping(value="enviar", method=RequestMethod.POST)
+	public String processarForm(String nome, String telefone, String email){
+		System.out.println(nome + " " + telefone + " " + email);
+		return "contato/form";
+	}
+	
+	//Usa-se o requestMapping e define os metodos(POST,GET) ou @GetMapping / @PostMapping
+
+//	@GetMapping("enviar")
+//	public String abrirForm() {
+//		return "contato/form";
+//	}
+
+//	@PostMapping(value="enviar")
+//	public String processarForm(String nome, String telefone, String email){
+//		System.out.println(nome + " " + telefone + " " + email);
+//		return "contato/form";
+//	}
+	
+}
